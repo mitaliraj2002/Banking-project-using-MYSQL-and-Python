2155beb21b430c4f31919b43255625c9cec01029
MITALI RAJ <113848695+mitaliraj2002@users.noreply.github.com>
2023-06-26 11:14:29 +0530

    bankproject.sql

--- bankproject.sql
+++ bankproject.sql
@@ -20,9 +20,9 @@ PRIMARY KEY ('acno')
 
 INSERT INTO 'customer'('acno','name','address','phone','email','aadhar_no','account_type','status','balance')
 values
-(1, 'RAVI RANJAN','CF-4 KOLKATA','7014515637','rishavsingh@gmail.com','1234-1243-4545','saving','active',250000.00),
-(2, 'RAJ KUMAR','PUMP NAGAR PATNA','9673431245','rajkumar@gmail.com','4545-1243-4565','current','active',150000.00),
-(3, 'MITTHU','6B-13 PEER TOLA','7668774612','mithhu12@gmail.com','1253-8765-2541','saving','active',165000.00);
+(1, 'RANJAN','CF-4 KOLKATA','7014515637','ranjansingh@gmail.com','1234-1243-4545','saving','active',250000.00),
+(2, 'NIRAJ KUMAR','PUMP NAGAR PATNA','9673431245','nirajkumar@gmail.com','4545-1243-4565','current','active',150000.00),
+(3, 'MITALI RAJ','6B-13 PEER TOLA','7668774612','mitaliraj12@gmail.com','1253-8765-2541','saving','active',165000.00);
 
 DROP TABLE IF EXISTS 'transaction';
 CREATE TABLE IF NOT EXISTS 'transaction'(
